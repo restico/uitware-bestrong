614188a6101cd903014d56166954e6aeeff7b9dc
restico <developer.restico@gmail.com>
2024-03-20 14:38:18 +0200

    Joined 2 pipelines into one, modified it to run on Microsoft hosted agents

--- provider.tf
+++ provider.tf
@@ -4,10 +4,10 @@ provider "azurerm" {
       prevent_cancellation_on_destroy = true
     }
   }
-
   # Replace this with environment variables on production
-  /*client_id       = var.bestrong-client-id
+  # When run localy, uncomment the following lines
+  client_id       = var.bestrong-client-id
   client_secret   = var.bestrong-client-secret
   tenant_id       = var.bestrong-tenant-id
-  subscription_id = var.bestrong-subscription-id*/
+  subscription_id = var.bestrong-subscription-id
 }
